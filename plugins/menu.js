f248e61b53fb37fdd7f21047242ae931ce3e9830
ismailhaddani32 <164524982+ismailhaddani32@users.noreply.github.com>
2024-03-24 02:18:08 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -145,7 +145,7 @@ renderLargerThumbnail: true
 }}}, { quoted: m})
 
     /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
-    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
+    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/x25__madara?igsh=OWllcnV1NTYzMGtw', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
       ['⏍ Info', `${_p}botinfo`],
       ['⌬ Grupos', `${_p}gpdylux`]
